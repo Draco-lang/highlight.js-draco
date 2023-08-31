e1abe27964cc9c7abd39faa3a91cba47fa7db2e4
LPeter1997 <lenkefi.peti@gmail.com>
2023-08-31 15:34:07 +0200

    Added keywords, comment

--- src/languages/draco.js
+++ src/languages/draco.js
@@ -1,12 +1,56 @@
 
 export default function(hljs) {
+    const KEYWORDS = [
+        'and',
+        'else',
+        'for',
+        'func',
+        'goto',
+        'if',
+        'import',
+        'in',
+        'intenral',
+        'mod',
+        'not',
+        'or',
+        'public',
+        'rem',
+        'return',
+        'val',
+        'var',
+        'while',
+    ];
+    const BUILT_INS = [
+        'bool',
+        'char',
+        'float32',
+        'float64',
+        'int8',
+        'int16',
+        'int32',
+        'int64',
+        'uint8',
+        'uint16',
+        'uint32',
+        'uint64',
+        'unit',
+        'string',
+    ];
+    const LITERALS = [
+        'true',
+        'false',
+    ];
+
     return {
         name: 'Draco',
         case_insensitive: false,
         keywords: {
-            keyword: 'func if else while for in',
-            literal: 'true false',
+            keyword: KEYWORDS,
+            built_in: BUILT_INS,
+            literal: LITERALS,
         },
-        contains: [],
+        contains: [
+            hljs.C_LINE_COMMENT_MODE,
+        ],
     };
 }
